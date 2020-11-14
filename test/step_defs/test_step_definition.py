cb00525f1247c258d19bf65c32d6c0d180b1c394
shubymao <le-li.mao@mail.mcgill.ca>
2020-11-14 02:31:43 -0500

    reformat the step defs

--- test/step_defs/test_step_definition.py
+++ test/step_defs/test_step_definition.py
@@ -17,7 +17,8 @@ def context():
 
 @given(parsers.parse('the system is running'))
 def theSystemIsSetup():
-    assert socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex(("localhost", 4567)) == 0
+    assert socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex(
+        ("localhost", 4567)) == 0
 
 @given(parsers.parse('the task with id {id} exist'))
 def checkOrCreateTodoWithID(id):
@@ -25,12 +26,12 @@ def checkOrCreateTodoWithID(id):
 
 @given(parsers.parse('the task with title "{title}" exists'))
 def checkOrCreateTodoWithTitle(title):
-    r = requests.get(url= f"http://localhost:4567/todos?title={title}")
+    r = requests.get(url=f"http://localhost:4567/todos?title={title}")
     if(len(r.json()['todos']) == 0):
         body = {'title': title}
         requests.post(url="http://localhost:4567/todos", json=body)
-    r = requests.get(url= f"http://localhost:4567/todos?title={title}")
-    assert len(r.json()['todos'])>0
+    r = requests.get(url=f"http://localhost:4567/todos?title={title}")
+    assert len(r.json()['todos']) > 0
     GLOBAL_ID.todoId = str(r.json()['todos'][0]['id'])
 
 @given(parsers.parse('the system contains a to do list with entry id {id}'))
@@ -67,16 +68,17 @@ def checkOrRemoveTodoDescription(id):
 @given(parsers.parse('the task has "{priority}" priority'))
 def checkOrSetTodoPriority(priority):
     checkOrCreateTodoPriority(priority)
-    body = {'id' : GLOBAL_ID.categoryId}
+    body = {'id': GLOBAL_ID.categoryId}
     url = f'http://localhost:4567/todos/{GLOBAL_ID.todoId}/categories'
     print("URL = " + url)
     r = requests.post(url=url, json=body)
     assert r.status_code == 201
-    r = requests.get(url = f'http://localhost:4567/todos/{GLOBAL_ID.todoId}/categories')
+    r = requests.get(
+        url=f'http://localhost:4567/todos/{GLOBAL_ID.todoId}/categories')
     assert r.status_code == 200
-    exist = len(list(filter(lambda x: x['id'] == GLOBAL_ID.categoryId, r.json()['categories']))) > 0
+    exist = len(list(
+        filter(lambda x: x['id'] == GLOBAL_ID.categoryId, r.json()['categories']))) > 0
     assert exist
-    
 
 
 @given(parsers.parse('the task with id {id} has description of "{description}"'))
@@ -116,7 +118,8 @@ def deleteNonExistentCategories(id):
 @when(parsers.parse('I add a new course with title <course>'))
 def add_category_no_conflict(course, context):
     # Add new course with title <course>
-    context['request_return'] = requests.post("http://localhost:4567/categories", json={"title": course})
+    context['request_return'] = requests.post(
+        "http://localhost:4567/categories", json={"title": course})
     assert context['request_return'].status_code == 201
 
 
@@ -126,16 +129,18 @@ def changeTaskPriority(priority):
     category = GLOBAL_ID.categoryId
     print(f"Category: {category}")
     print(f"todo: {todo}")
-    r = requests.delete(url = f'http://localhost:4567/todos/{todo}/categories/{category}')
+    r = requests.delete(
+        url=f'http://localhost:4567/todos/{todo}/categories/{category}')
     assert r.status_code == 200
     checkOrCreateTodoPriority(priority)
     category = GLOBAL_ID.categoryId
-    body = { 'id' : category }
-    r = requests.post(url=f'http://localhost:4567/todos/{todo}/categories', json=body)
+    body = {'id': category}
+    r = requests.post(
+        url=f'http://localhost:4567/todos/{todo}/categories', json=body)
     assert r.status_code == 201
 
 @when(parsers.parse('the student change task with {id} to "{priority}" priority'))
-def changeTaskPriorityWithID(id,priority):
+def changeTaskPriorityWithID(id, priority):
     assert True
 
 
@@ -167,7 +172,6 @@ def addTodoToCategory(course, context):
 def getCategory():
     raise NotImplementedError
 
-
 @then(parsers.parse('I should see a success message'))
 def assertSuccessOperation(context):
     assert context['request_return'].status_code in [200, 201]
@@ -193,16 +197,15 @@ def getCategories(course, context):
             return
     assert False
 
-
 @then(parsers.parse('the priority of the task should be "{priority}"'))
 def checkIfTaskHasPriority(priority):
     todo = GLOBAL_ID.todoId
-    r = requests.get(url = f"http://localhost:4567/todos/{todo}/categories")
+    r = requests.get(url=f"http://localhost:4567/todos/{todo}/categories")
     assert r.status_code == 200
     categories = r.json()['categories']
-    exist = len(list(filter (lambda x : x['id'] == GLOBAL_ID.categoryId, categories))) > 0
+    exist = len(
+        list(filter(lambda x: x['id'] == GLOBAL_ID.categoryId, categories))) > 0
     assert exist
-
 
 @then(parsers.parse('the system shall inform the user that the task doesn\'t exist'))
 def checkIfMessageEqualsTaskDoNotExist():
@@ -233,7 +236,8 @@ def checkUniquenessCategory(course):
 
 @then(parsers.parse('I should see a new task in <course>'))
 def checkTaskAdded(course, context):
-    r = requests.get("http://localhost:4567/categories/['request_return']['id']/projects")
+    r = requests.get(
+        "http://localhost:4567/categories/['request_return']['id']/projects")
     for project in r.json()['projects']:
         if project['title'] == context['task_return'].json()['title'] and \
                 project['id'] == context['task_return'].json()['id']:
@@ -244,10 +248,10 @@ def checkTaskAdded(course, context):
 # Helper functions
 
 def checkOrCreateTodoPriority(priority):
-    r = requests.get(url= f"http://localhost:4567/categories?title={priority}")
+    r = requests.get(url=f"http://localhost:4567/categories?title={priority}")
     if(len(r.json()['categories']) == 0):
         body = {'title': priority}
         requests.post(url="http://localhost:4567/categories", json=body)
-    r = requests.get(url= f"http://localhost:4567/categories?title={priority}")
+    r = requests.get(url=f"http://localhost:4567/categories?title={priority}")
     GLOBAL_ID.categoryId = str(r.json()['categories'][0]['id'])
-    assert len(r.json()['categories'])>0
+    assert len(r.json()['categories']) > 0
