3dfc59e078da401adf76679db41b237d41e856b4
Yuyu <8889046+Yuyu-Ren@users.noreply.github.com>
2020-11-14 23:04:13 -0500

    Adds timeout requirements and updates test to timeout if the jar never comes back online

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import requests
 import socket
+import time
 
 class GLOBAL_CONTEXT:
     todo_id = None
@@ -10,14 +11,22 @@ class GLOBAL_CONTEXT:
     response_json = None
     status_code = None
 
+
 @pytest.fixture(autouse=True, scope="function")
 def setup():
+    # Attempts to start the process.
     subprocess.Popen(['java', '-jar', os.environ['JAR_PATH']])
+    # Start timer for timeout which is current time + 3 seconds for timeout
+    timeout = time.time() + 3
+    # We loop until we either reach a time upper bound, or a socket with this program's expected port opens
     while socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect_ex(("localhost", 4567)) != 0:
-        continue
+        # It's not gonna come up by this point :(
+        if time.time() > timeout:
+            raise ConnectionError("Cannot connect to program/server. Test cannot continue")
+    # Loop terminates here because the socket connection has opened. Start testing.
     yield
+    # This section corresponds to post-test teardown.
     try:
         requests.get("http://localhost:4567/shutdown")
-        pass
     except Exception:
         pass
