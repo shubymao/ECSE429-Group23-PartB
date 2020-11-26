c9470e10e70440d99cd7a4d7733dcd0a0191818d
Yuyu <8889046+Yuyu-Ren@users.noreply.github.com>
2020-11-25 21:04:15 -0500

    adds new requirement for pytest-benchmark

--- test/performance_tests/test_performance.py
+++ test/performance_tests/test_performance.py
@@ -1,8 +1,9 @@
 import requests
 
 
-def test_todos_performance():
-    raise NotImplemented
+def test_todos_performance(benchmark):
+    todo_values = [10, 100, 1000, 100000, 100000]
+
 
 
 def test_categories_performance():
